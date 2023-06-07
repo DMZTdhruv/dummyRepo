4cca897e28826455a0c6635858400be79b2b0623
DMZTdhruv <shruvparmar.gmail.com>
2023-06-07 15:47:46 +0530

    learning about packagaes

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ const loadash = require('lodash');
 const items = [1, [2 , [3 , [4]]]];
 const newItems = loadash.flatMapDeep(items);
 console.log(newItems);
-console.log('Hello, world');
+console.log('Lol let me restart the app');
+
+//"scripts" 
+// "dev" : to run the app as npm run dev
+// "start" : nodemon app.js if you want to run it without approaching npm run dev!
